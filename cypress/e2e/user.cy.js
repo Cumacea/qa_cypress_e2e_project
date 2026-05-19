cb9633c0407a0ff6b7f3ce2d81f346a93c636baf
Yurii Kumpanenko <yuriikumpanenko@ukr.net>
2026-05-19 16:43:23 +0300

    add task solution

--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -1,12 +1,44 @@
 /// <reference types='cypress' />
 /// <reference types='../support' />
 
-describe('User', () => {
-  before(() => {
+import SignInPageObject from '../support/pages/signIn.pageObject';
 
+const signInPage = new SignInPageObject();
+
+describe('User', () => {
+  let userTarget;
+  let userFollower;
+
+  before(() => {
+    cy.task('db:clear');
+
+    cy.task('generateUser').then((generatedUser) => {
+      userTarget = generatedUser;
+      cy.register(userTarget.email, userTarget.username, userTarget.password);
+    });
+
+    cy.task('generateUser').then((generatedUser) => {
+      userFollower = generatedUser;
+      cy.register(
+        userFollower.email,
+        userFollower.username,
+        userFollower.password
+      );
+    });
   });
 
-  it.skip('should be able to follow the another user', () => {
+  it('should be able to follow the another user', () => {
+    signInPage.visit();
 
+    signInPage.typeEmail(userFollower.email);
+    signInPage.typePassword(userFollower.password);
+    signInPage.clickSignInBtn();
+
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(1500);
+
+    cy.visit(`/#/@${userTarget.username}`);
+
+    cy.contains('button', `Follow ${userTarget.username}`).click();
   });
 });
