444ec4abb1430f8c438ef9d22d612248904edb3a
Yurii Kumpanenko <yuriikumpanenko@ukr.net>
2026-05-21 10:50:24 +0300

    fix cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('cypress');
 const { faker } = require('@faker-js/faker');
-const { clear } = require('./server/db');
-const { seed } = require('./server/db');
 const {
   addMatchImageSnapshotPlugin
 } = require('cypress-image-snapshot/plugin');
@@ -28,11 +26,15 @@ module.exports = defineConfig({
           };
         },
         async 'db:clear'() {
+          const { clear } = require('./server/db');
+
           await clear();
 
           return null;
         },
         async 'db:seed'() {
+          const { seed } = require('./server/db');
+
           await seed();
 
           return null;
